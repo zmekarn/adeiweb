aadff5042c294eee26e23770f97f6ad644e6fc96
Magnus Adler <magnus.ad@hotmail.com>
2020-02-19 20:01:22 +0100

    important

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ricardo from '../public/ricardo2.png'
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={ricardo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          eat ass
         </p>
       </div>
     );
