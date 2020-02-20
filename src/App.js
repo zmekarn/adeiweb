7f356675ca982c21652c26a57661ebceaa10aa45
Magnus Adler <magnus.ad@hotmail.com>
2020-02-20 19:11:05 +0100

    ting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import ricardo from '../public/ricardo2.png'
+import ting from '../public/ting2.svg';
+import bg from '../public/coding.jpg';
 import './App.css';
+import Logo from './icons/Logo';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topbarSolid: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log("suh")
+    window.addEventListener('scroll', (ev) => {
+      const { scrollY } = ev.path[1];
+      if (scrollY > 0 && !this.state.topbarSolid) {
+        console.log("ja");
+        this.setState({topbarSolid: true});
+      } else if (scrollY === 0 && this.state.topbarSolid) {
+        console.log("nei");
+        this.setState({topbarSolid: false})
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={ricardo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className={`topbar ${this.state.topbarSolid ? 'solid' : null}`}>
+          <div className="topbar-logo">
+            <Logo color={'white'} size={5} />
+          </div>
+          <div className="topbar-menu">
+            <span>HOME</span>
+            <span>ABOUT</span>
+            <span>CONTACT</span>
+          </div>
         </div>
-        <p className="App-intro">
-          eat ass
-        </p>
+        <div className="top">
+          <span className="top-descr">We are ADEI</span>
+        </div>
+        <div className="bot"></div>
+        <div>
+          
+        </div>
       </div>
     );
   }
