5c0341b4265793195d22950c5c8676827a3dc23e
Eirik the E <eilertseneirik@gmail.com>
2020-02-20 21:50:21 +0100

    yee bby

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    document.title = "ADEI"
     window.addEventListener('scroll', (ev) => {
       const { scrollY } = ev.path[1];
       if (scrollY > 0 && !this.state.topbarSolid) {
